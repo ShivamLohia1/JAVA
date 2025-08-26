6a394bbcdb8eb847a9235196f8c8bd661733a5f2
Shivam Lohia <contactshivamlohia@gmail.com>
2025-08-26 21:38:24 +0530

    Add files via upload

---
+++ lec_0.java
@@ -0,0 +1,18 @@
+
+public class lec_0{
+    public static void main(String[] args) {
+        int A =9;
+        float b =9.25f;
+        char c = '@';
+        String str = "SHIVAM";
+        System.out.println("Integer a ="+A);
+        System.out.println ("Float b ="+ b);
+        System.out.println("Character c ="+c);
+        System.out.println("String is ="+ str);
+        // int a=14,b=7;
+        // System.out.println("Addition is = "+(a+b));
+        // System.out.println("Subtraction is = "+(a-b));
+        // System.out.println("Multiplication is = "+(a*b));
+        // System.out.println("Division is = "+(a/b));
+    }
+}
