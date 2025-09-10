43e6134d257de7d769aa8ba93387cd2c9c2ca180
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-10 11:27:22 +0530

    Add files via upload

---
+++ Function_object.java
@@ -0,0 +1,11 @@
+// package L4;
+
+public class Function_object {
+    public void print(){
+        System.out.println("This is print function.");
+    }
+    public static void main(String[] args) {
+        Function_object obj = new Function_object();
+        obj.print();
+    }
+}q
---
+++ No_of_digit.java
@@ -0,0 +1,19 @@
+// package Lecture_04_Function;
+import java.util.*;
+
+
+public class No_of_digit {
+    public static int digit(int n){
+        int count = 0;
+        while(n!=0){
+            n/=10;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(digit(n));
+    }    
+}
