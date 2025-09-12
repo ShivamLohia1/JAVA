7f81f2aa07a431d10ea56331ffdfa0f9dbb1f9c7
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-12 12:27:31 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,14 @@
+public class arrays {
+    public static void main(String[] args) {
+        int arr [] = {10,20,-4,0,1};
+
+        for (int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        System.out.println();
+
+        for(int element : arr){
+            System.out.println(element);
+        }
+    }
+}
