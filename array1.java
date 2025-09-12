2fc5960c5fa5deb5e2fe0a355cb0d701c9bf7816
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-12 12:27:13 +0530

    Add files via upload

---
+++ array1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//package array;
+public class array1 {
+    public static int findMax(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+        return Math.max(arr[n - 1], findMax(arr, n - 1));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int max = findMax(arr, n);
+        System.out.println("The maximum element in the array is: " + max);
+        sc.close();
+    }
+}
---
+++ array_input.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class array_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the lenght of array");
+        int n = sc.nextInt();
+
+        int nums [] = new int[n];
+
+        System.out.println("Enter the elements of array");
+        for(int i=0; i<n; i++){
+            nums [i] = sc.nextInt();
+        }
+        sc.close();
+        System.out.println("Array Elements are:");
+        for(int x : nums){System.out.println(x+" ");
+        }
+    }
+}
