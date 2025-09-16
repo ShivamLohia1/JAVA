29f834bfb2fac0c9b0010dbfa6b1b4091b1e55ce
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-16 09:23:06 +0530

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,15 @@
+// package Lecture_07_Searching.Linear;
+
+public class Intro {
+    static int search(int[] arr,int target){
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]==target) return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,-1,0,5,6,9};
+        int target = 6;
+        System.out.println(search(arr, target));
+    }
+}
