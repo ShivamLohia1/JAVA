7d9bebfadde5349814c1bdc0262ec0a95b1ab467
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-18 09:26:44 +0530

    Update array_input.java

--- array_input.java
+++ array_input.java
@@ -4,9 +4,7 @@ public class array_input {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the lenght of array");
         int n = sc.nextInt();
-
         int nums [] = new int[n];
-
         System.out.println("Enter the elements of array");
         for(int i=0; i<n; i++){
             nums [i] = sc.nextInt();
@@ -16,4 +14,5 @@ public class array_input {
         for(int x : nums){System.out.println(x+" ");
         }
     }
+
 }
