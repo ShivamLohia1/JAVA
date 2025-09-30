37fdcff49c5b7f87e81300751cdde1324fce0c62
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-30 10:42:53 +0530

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -8,7 +8,6 @@ public class bubble_sort {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
@@ -25,4 +24,5 @@ public class bubble_sort {
         }
         sc.close();
     }
+
 }
